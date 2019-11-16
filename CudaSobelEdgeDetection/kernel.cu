8bd5f150850425b283b487bcdf194860c99e63a8
Soma Makai <soma.makai@gmail.com>
2019-11-16 19:12:27 +0100

    bla

--- CudaSobelEdgeDetection/kernel.cu
+++ CudaSobelEdgeDetection/kernel.cu
@@ -15,10 +15,46 @@
 //minden block 2 dimenziós lesz, 32*32 így minden block 1024 szálat tud futtatni egy időben
 //ez annyit jelent, hogy egy 4000*4000-es képnél 125 block vízszintesen, 125 függőlegesen
 
-unsigned int width, height;
+unsigned int output[4000 * 4000 * 3];
 
-__device__ unsigned char* dev_orig;
-__device__ unsigned char* dev_result;
+unsigned int width, height;
+unsigned int imgres[IMG_HEIGHT][IMG_WIDTH];
+unsigned int imageMatrix[IMG_HEIGHT][IMG_WIDTH];
+
+unsigned int resultImage[IMG_HEADER + IMG_WIDTH*IMG_HEIGHT];
+
+__device__ unsigned int dev_orig[IMG_HEIGHT][IMG_WIDTH];
+__device__ unsigned int dev_result[IMG_HEIGHT][IMG_WIDTH];
+
+void readBMP(char* filename)
+{
+	int i;
+	FILE* f = fopen(filename, "rb");
+	unsigned char info[54];
+	fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+	for (size_t i = 0; i < 54; i++)
+	{
+		resultImage[i] = info[i];
+	}
+
+	// extract image height and width from header
+	int width = *(int*)&info[18];
+	int height = *(int*)&info[22];
+
+	int size = width * height*3;
+	
+	fread(output, sizeof(unsigned int), size*3, f); // read the rest of the data at once
+	fclose(f);
+
+	for (size_t i = 0; i < IMG_HEIGHT; i++)
+	{
+		for (size_t j = 0; j < IMG_WIDTH; j++)
+		{
+			imageMatrix[i][j] = output[i * IMG_HEIGHT + j + 3];
+		}
+	}
+}
 
 
 __global__ void EdgeDetect(int width, int height) {
@@ -37,7 +73,7 @@ __global__ void EdgeDetect(int width, int height) {
 
 	for (int i = -1; i <= 1; i++) {
 		for (int j = -1; j <= 1; j++) {
-			int curPixel = dev_orig[(row + j) * width + (col + i)];
+			int curPixel = dev_orig[row + j][col + i];
 			sumX += curPixel * Gx[i + 1][j + 1];
 			sumY += curPixel * Gy[i + 1][j + 1];
 		}
@@ -47,7 +83,7 @@ __global__ void EdgeDetect(int width, int height) {
 	if (sum > 255) sum = 255;
 	if (sum < 0) sum = 0;
 
-	dev_result[row * width + col] = sum;
+	dev_result[row][col] = dev_orig[row][col];
 }
 
 
@@ -55,43 +91,40 @@ int main()
 {
 	printf("Starting program \n\n");
 
-	unsigned char* img; //can't store negative values
-	FILE* f_input_img, * f_output_img;
+	FILE * f_output_img;
 
-	//load image rész
-	//dinamikus memória allokáció
-	img = (unsigned char*)malloc(IMG_HEADER + sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
-	
-	// fájl kinyit; param: A pointer to the file pointer that will receive the pointer to the opened file, filename, mode: ebben az esetben read)
-	fopen_s(&f_input_img, IMG_INPUT, "rb"); 
-	//Reads an array of count elements, each one with a size of size bytes, from the stream and stores them in the block of memory specified by ptr.
-	fread(img, 1, IMG_HEADER + IMG_WIDTH * IMG_HEIGHT, f_input_img);
-	//Closes the file associated with the stream and disassociates it.
-	fclose(f_input_img);
+	readBMP(IMG_INPUT);
 
 	//load image to CUDA managed memory
-	cudaMemcpyToSymbol(dev_orig, img, IMG_HEADER + sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
-	cudaMemcpyToSymbol(dev_result, img, IMG_HEADER + sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
+	cudaMemcpyToSymbol(dev_orig, imageMatrix, sizeof(unsigned int) * IMG_WIDTH * IMG_HEIGHT);
 
 	CPreciseTimer timer;
 	for (int _timeri = 0; _timeri <= 1; _timeri++) {
-			if (_timeri == 1)
-				timer.StartTimer();
-				EdgeDetect << <dim3(125, 125), dim3(32,32) >> > (IMG_WIDTH, IMG_HEIGHT);
+		if (_timeri == 1)
+			timer.StartTimer();
+		EdgeDetect << <dim3(125, 125), dim3(32, 32) >> > (IMG_WIDTH, IMG_HEIGHT);
 	}
 	timer.StopTimer();
 
 	printf("Timer[%s]=%f\n\n", "Edge decetion", (float)timer.GetTimeMilliSec() / 1);
+
+	cudaMemcpyFromSymbol(imgres, dev_result, sizeof(unsigned int) * IMG_WIDTH * IMG_HEIGHT);
+
+	for (size_t i = 0; i < IMG_HEIGHT; i++)
+	{
+		for (size_t j = 0; j < IMG_WIDTH; j++)
+		{
+			resultImage[(i * 4000 + j) + 54] = imgres[i][j];
+		}
+	}
 	
-	cudaMemcpyFromSymbol(img, &dev_result, IMG_HEADER + sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
 
 	// Save file
 	fopen_s(&f_output_img, IMG_OUTPUT2, "wb");
-	fwrite(img, 1, IMG_HEADER + IMG_WIDTH * IMG_HEIGHT, f_output_img);
+	fwrite(resultImage, 1, IMG_HEADER + IMG_WIDTH * IMG_HEIGHT, f_output_img);
 	fclose(f_output_img);
 
 	// Free CUDA managed host memory
-	cudaFreeHost(img);
 
 	printf("Ending program \n");
 }
