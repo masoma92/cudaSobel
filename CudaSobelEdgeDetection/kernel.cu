60f76ab708feb055d201c90a71c6bf5f25676e14
Soma Makai <soma.makai@gmail.com>
2019-11-20 20:28:20 +0100

    refactoring

--- CudaSobelEdgeDetection/kernel.cu
+++ CudaSobelEdgeDetection/kernel.cu
@@ -10,16 +10,16 @@
 // lady 1080; 512; 512; 16; 16; 32; 32
 // lena 1080; 512; 512; 16; 16; 32; 32
 
-#define IMG_INPUT "img\\baby.bmp"
+#define IMG_INPUT "img\\lena.bmp"
 #define IMG_OUTPUT "img\\outputSingle.bmp"
 #define IMG_OUTPUT2 "img\\outputMultiple.bmp"
 
 #define IMG_HEADER 1080
-#define IMG_WIDTH 4000
-#define IMG_HEIGHT 4000
+#define IMG_WIDTH 512
+#define IMG_HEIGHT 512
 
-#define BLOCKSIZE_X 125
-#define BLOCKSIZE_Y 125
+#define BLOCKSIZE_X 16
+#define BLOCKSIZE_Y 16
 #define THREAD_X 32
 #define THREAD_Y 32
 
@@ -30,7 +30,7 @@
 __global__ void EdgeDetectionKernel(unsigned char* img, unsigned char* img_output)
 {
 	//ez felel meg a szekvenciális kódban a két egybeágyazott for ciklusnak
-	int row = blockIdx.y * blockDim.y + threadIdx.y; //i blockidx a hanyadik block az oszlopban, blockdim az a blokkon belüli sor
+	int row = blockIdx.y * blockDim.y + threadIdx.y; //i blockidx a hanyadik block az oszlopban, blockdim.y = 32 thread db szám vízszintesen
 	int col = blockIdx.x * blockDim.x + threadIdx.x; //j
 
 	if (row >= IMG_HEIGHT || col >= IMG_WIDTH || row < 1 || col < 1) return; //olyan szál le se fusson ami kiindexelne a képből
@@ -45,8 +45,8 @@ __global__ void EdgeDetectionKernel(unsigned char* img, unsigned char* img_outpu
 	for (int i = -1; i <= 1; i++) {
 		for (int j = -1; j <= 1; j++) {
 			char curPixel = img[(row + j) * IMG_WIDTH + (col + i)];
-			sumX += (curPixel + '0') * Gx[i + 1][j + 1];
-			sumY += (curPixel + '0') * Gy[i + 1][j + 1];
+			sumX += (curPixel) * Gx[i + 1][j + 1];
+			sumY += (curPixel) * Gy[i + 1][j + 1];
 		}
 	}
 
@@ -57,7 +57,7 @@ __global__ void EdgeDetectionKernel(unsigned char* img, unsigned char* img_outpu
 	if (sum < 0) 
 		sum = 0;
 
-	img_output[row * IMG_WIDTH + col] = sum - '0';
+	img_output[row * IMG_WIDTH + col] = sum;
 }
 
 void EdgeDetectionCaller(unsigned char* img, unsigned char* img_output)
@@ -93,31 +93,32 @@ void EdgeDetectionSequential(unsigned char* input_img, unsigned char* output_img
 	int Gx[3][3] = { {-1,0,1}, {-2,0,2}, {-1,0,1} };
 	int Gy[3][3] = { {1,2,1}, {0,0,0}, {-1,-2,-1} };
 
-	double sumX = 0;
-	double sumY = 0;
-	double sum = 0;
+	int sumX = 0;
+	int sumY = 0;
+	int sum = 0;
 
-	for (size_t i = IMG_HEADER+IMG_WIDTH; i < IMG_HEIGHT*IMG_WIDTH-IMG_WIDTH; i++)
+	for (int i = IMG_HEADER + IMG_WIDTH; i < (IMG_HEIGHT*IMG_WIDTH)-IMG_WIDTH; i++) //első és utolsó sornyi pixelt kihagyja
 	{
-		if ((i - IMG_HEADER) % (IMG_WIDTH+1) != 0 || (i - IMG_HEADER) % (IMG_WIDTH+1) != 1) {
+		if (((i - IMG_HEADER) % (IMG_WIDTH)) != 0 && ((i - IMG_HEADER) % (IMG_WIDTH)) != IMG_WIDTH-1) { //széleket kihagyja
 
 			for (int k = -1; k <= 1; k++) {
 				for (int l = -1; l <= 1; l++) {
 
 					char curPixel = input_img[i + (k * IMG_WIDTH) + l];
-					sumX += (curPixel + '0') * Gx[k + 1][l + 1];
-					sumY += (curPixel + '0') * Gy[k + 1][l + 1];
+					sumX += (curPixel) * Gx[k + 1][l + 1];
+					sumY += (curPixel) * Gy[k + 1][l + 1];
 				}
 			}
 		}
 
 		sum = sqrt(pow(sumY, 2) + pow(sumX, 2));
+
 		if (sum > 255)
 			sum = 255;
 		if (sum < 0)
 			sum = 0;
 
-		output_img[i] = sum - '0';
+		output_img[i] = sum;
 	}
 
 	
