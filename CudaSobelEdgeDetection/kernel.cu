eb73987ad3c33788f4094fb4c08364bb980acde3
Soma Makai <soma.makai@gmail.com>
2019-11-25 13:00:08 +0100

    finialize

--- CudaSobelEdgeDetection/kernel.cu
+++ CudaSobelEdgeDetection/kernel.cu
@@ -5,10 +5,12 @@
 #include <math.h>
 #include "res\Timing.h"
 
-// Image data
-// baby 1080; 4000; 4000; 125; 125; 32; 32
-// lady 1080; 512; 512; 16; 16; 32; 32
-// lena 1080; 512; 512; 16; 16; 32; 32
+/** HELPER
+** Image data
+** baby 1080; 4000; 4000; 125; 125; 32; 32
+** lady 1080; 512; 512; 16; 16; 32; 32
+** lena 1080; 512; 512; 16; 16; 32; 32 
+**/
 
 #define IMG_INPUT "img\\baby.bmp"
 #define IMG_OUTPUT "img\\outputSingle.bmp"
@@ -23,16 +25,17 @@
 #define THREAD_X 32
 #define THREAD_Y 32
 
-//minden block 2 dimenziós lesz, 32*32 így minden block 1024 szálat tud futtatni egy időben
-//ez annyit jelent, hogy egy 4000*4000-es képnél 125 block vízszintesen, 125 függőlegesen
+// minden block 2 dimenziós, 32*32 szál/block -> 1024 szál egy időben
+// ez egy 4000*4000-es képnél 125 block vízszintesen, 125 függőlegesen
 
 
 __global__ void EdgeDetectionKernel(unsigned char* input_img, unsigned char* output_img)
 {
-	//ez felel meg a szekvenciális kódban a két egybeágyazott for ciklusnak
-	int row = blockIdx.y * blockDim.y + threadIdx.y; //i blockidx a hanyadik block az oszlopban, blockdim.y = 32 thread db szám vízszintesen
+	// Ez felel meg a szekvenciális kódban a két egybeágyazott for ciklusnak
+	int row = blockIdx.y * blockDim.y + threadIdx.y; //i blockidx a hanyadik block a az x/y tengelyen, blockdim -> 32 x/y tengely blockon belül
 	int col = blockIdx.x * blockDim.x + threadIdx.x; //j
 
+	// Szűrő lsd.: documentation_hun.pdf
 	int Gx[3][3] = { {-1,0,1}, {-2,0,2}, {-1,0,1} };
 	int Gy[3][3] = { {1,2,1}, {0,0,0}, {-1,-2,-1} };
 
@@ -40,6 +43,7 @@ __global__ void EdgeDetectionKernel(unsigned char* input_img, unsigned char* out
 	double sumY = 0;
 	double sum = 0;
 
+	// Szűrő alkalmazása az adott blockon belüli képrészletre
 	for (int i = -1; i <= 1; i++) {
 		for (int j = -1; j <= 1; j++) {
 
@@ -58,27 +62,27 @@ __global__ void EdgeDetectionKernel(unsigned char* input_img, unsigned char* out
 
 void EdgeDetectionCaller(unsigned char* img, unsigned char* img_output)
 {
-	//Allocate device memory
 	unsigned char* d_img;
 	unsigned char* d_img_output;
 
+	// Gpu memória allokáció
 	cudaMalloc((void**)&d_img, sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
 	cudaMalloc((void**)&d_img_output, sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
 
-	//Memory copy H->D
+	// Másolás H->D
 	cudaMemcpy(d_img, img + IMG_HEADER, sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT, cudaMemcpyHostToDevice);
 
 
-	//Launch kernel
+	// Kernel futtatás
 	EdgeDetectionKernel << < dim3(BLOCKSIZE_X, BLOCKSIZE_Y), dim3(THREAD_X, THREAD_Y) >> > (d_img, d_img_output);
 
 
-	//Memory copy D->H
+	// Másolás D->H
 	cudaMemcpy(img + IMG_HEADER, d_img, sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT, cudaMemcpyDeviceToHost);
 	cudaMemcpy(img_output + IMG_HEADER, d_img_output, sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT, cudaMemcpyDeviceToHost);
 
 
-	//Free device memory
+	// Memória felszabadítás
 	cudaFree(d_img);
 	cudaFree(d_img_output);
 }
@@ -127,7 +131,7 @@ int main()
 	unsigned char* img_output;
 	FILE* f_input_img, * f_output_img;
 
-	// Load image
+	// Kép betöltése
 	img = (unsigned char*)malloc(IMG_HEADER + sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
 	img_output = (unsigned char*)malloc(IMG_HEADER + sizeof(unsigned char) * IMG_WIDTH * IMG_HEIGHT);
 
@@ -139,16 +143,17 @@ int main()
 	fread(img_output, 1, IMG_HEADER + IMG_WIDTH * IMG_HEIGHT, f_input_img);
 	fclose(f_input_img);
 
+	// Szekvenciális megoldás futási idejének mérése
 	MEASURE_TIME(1, "EdgeDetectionSequential", EdgeDetectionSequential(img + IMG_HEADER, img_output + IMG_HEADER));
 
 	fopen_s(&f_output_img, IMG_OUTPUT, "wb");
 	fwrite(img_output, 1, IMG_HEADER + IMG_WIDTH * IMG_HEIGHT, f_output_img);
 	fclose(f_output_img);
 
-	// Run cuda kernel
+	// Cuda kernel megoldás futási idejének mérése
 	MEASURE_TIME(1, "EdgeDetectionParallelKernel", EdgeDetectionCaller(img, img_output));
 
-	// Save file
+	// Fájl mentése
 	fopen_s(&f_output_img, IMG_OUTPUT2, "wb");
 	fwrite(img_output, 1, IMG_HEADER + IMG_WIDTH * IMG_HEIGHT, f_output_img);
 	fclose(f_output_img);
